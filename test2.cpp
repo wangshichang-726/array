3dc1bb567cf5c35f7d8035d03484a41cb8fb0869
wsc <wsc5171372337@outlook.com>
2023-08-31 21:50:39 +0800

    更改描述：test2，二分查找

--- test2.cpp
+++ test2.cpp
@@ -1,15 +1,63 @@
-#include <iostream>
-using namespace std;
+/**
+ * @file test2.cpp
+ * @author wsc
+ * @brief 给定一个 n 个元素有序的（升序）整型数组 nums 和一个目标值 target，
+ * 写一个函数搜索 nums 中的 target，如果目标值存在返回下标，否则返回 -1。
+ * 输入: nums = [-1,0,3,5,9,12], target = 9
+ * 输出: 4
+ * 解释: 9 出现在 nums 中并且下标为 4
+ * @version 使用线性查找方法
+ * @date 2023-08-31
+ */
+/*
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        for (int i = 0; i<nums.size(); i++ ){
+            if (target==nums[i]){
+                return i;
+            }else{
+                continue;
+            }
+        }
+        return -1;
 
-void test_arr() {
-    int array[2][3] = {
-        {0, 1, 2},
-        {3, 4, 5}
-    };
-    cout << &array[0][0] << " " << &array[0][1] << " " << &array[0][2] << endl;
-    cout << &array[1][0] << " " << &array[1][1] << " " << &array[1][2] << endl;
-}
+    }
+};
+*/
+
+/**
+ * @file test2.cpp
+ * @author wsc
+ * @version 有序数列使用二分查找
+ * @date 2023-08-31
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+#include <iostream>
+#include <vector>
+
+class Solution {
+public:
+    int search(std::vector<int>& nums, int target) {
+        for (int i = 0; i < nums.size(); i++) {
+            if (target == nums[i]) {
+                return i;
+            } else {
+                continue;;
+            }
+        }
+        return -1;
+    }
+};
 
 int main() {
-    test_arr();
+    Solution solution;
+    std::vector<int> nums = {0, 1, 2, 4, 5, 6, 7};
+    int target = 4;
+    int result = solution.search(nums, target);
+    std::cout<<result;
+    return 0;
 }
+
